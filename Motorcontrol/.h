f3c9e89ebe319f89694f0f2decfaba8d8df84e33
javierpadila2001 <73498851+javierpadila2001@users.noreply.github.com>
2020-11-14 13:41:56 -0600

    Update .h

--- Motorcontrol/.h
+++ Motorcontrol/.h
@@ -1,1 +1,6 @@
-This code inherit everything from MotorController.c and then use this information to overide and apply for another things
+#This code inherit everything from MotorController.c and then use this information to overide and apply for another things
+
+
+CANID() = return CANID;
+
+CANMESSAGE() = return CARMESSAGE;
