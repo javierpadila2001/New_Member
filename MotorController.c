169337f8b6d841058b728529425d16a49b37d44e
javierpadila2001 <73498851+javierpadila2001@users.noreply.github.com>
2020-11-14 13:31:57 -0600

    Delete MotorController.c

--- MotorController.c
+++
@@ -1,9 +0,0 @@
-// This will write to the CAN.csv with format"0xCAN_ID, CAN_MESSAGE". It should accpect both the "CAN_ID" and the "CAN_MESSAGE" as parameters.
-/*
-
-char CAN_ID = "0x_CAN_ID";
-
-char CA_MESSAGE = "CAN_MESSAGE";
-
-
-*/
